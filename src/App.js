b50aef089bc80fc38ec18aed5fc32d4088e2552a
Michael <michaelshep52@gmail.com>
2023-08-20 08:53:47 -0400

    Bootstrap designs added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import Badge from "react-bootstrap/Badge";
 import { marked } from 'marked';
 import React from 'react';
 const markdown=`    
-# H1
-## H2
-\
-[title](https://www.example.com)
+# Heading level 1
+## Heading level 2
+### Heading level 3
 
+I love supporting the **[EFF](https://eff.org)**.
+This is the *[Markdown Guide](https://www.markdownguide.org)*.
 \`code\`
 
 \`\`\`
@@ -16,16 +17,38 @@ const markdown=`
   "age": 25
 }
 \`\`\`
+<html>
+  <head>
+    <title>Test</title>
+  </head>
+  <body>
+    <h1></h1>
+  </body>
+</html>
 
-- First item
-- Second item
-- Third item
+1. First item
+2. Second item
+3. Third item
+4. Fourth item
+* First item
+* Second item
+* Third item
+* Fourth item
 
 > blockquote
+>> blockquote in a blockquote
 
-![alt text](image.jpg)
+[![An old rock in the desert](/assets/images/shiprock.jpg "Shiprock, New Mexico by Beau Rogers")](https://www.flickr.com/photos/beaurogers/31833779864/in/photolist-Qv3rFw-34mt9F-a9Cmfy-5Ha3Zi-9msKdv-o3hgjr-hWpUte-4WMsJ1-KUQ8N-deshUb-vssBD-6CQci6-8AFCiD-zsJWT-nNfsgB-dPDwZJ-bn9JGn-5HtSXY-6CUhAL-a4UTXB-ugPum-KUPSo-fBLNm-6CUmpy-4WMsc9-8a7D3T-83KJev-6CQ2bK-nNusHJ-a78rQH-nw3NvT-7aq2qf-8wwBso-3nNceh-ugSKP-4mh4kh-bbeeqH-a7biME-q3PtTf-brFpgb-cg38zw-bXMZc-nJPELD-f58Lmo-bXMYG-bz8AAi-bxNtNT-bXMYi-bXMY6-bXMYv)
 
 **bold text**
+I just love __bold text__.
+Love**is**bold
+
+This is the first line  
+
+And this is the second line..  
+
+
 `;
   
 
@@ -44,11 +67,11 @@ render(){
     height: "50vh",
     marginLeft: "auto",
     marginRight: "auto",
-    padding: "10px",
+    padding: "0",
   };
   var outputStyle = {
     width: "100%",
-    height: "100%",
+    height: "90%",
     backgroundColor: "#DCDCDC",
     marginLeft: "auto",
     marginRight: "auto",
@@ -57,24 +80,24 @@ render(){
   };
 
   return (
-    <div className="App">
-      <div className="container">
-        <div className="row mt-4">
+    <div className="App" class="bg-info">
+      <div className="container-fluid">
+        <div className="row justify-content-center">
           <div className="col text-center">
             <h1>
               {""}
-            <Badge className="text-align-center" variant="light">
+            <Badge className="text-align-center mt-4 bg-secondary" variant="light">
                 Markdown Previewer
             </Badge>
             </h1>
           </div>
         </div>
-        <div className="row mt-4">
-          <div className="col-md-6">
+        <div className="row mt-4 justify-content-center">
+          <div className="col-md-7 shadow-lg">
             {""}
             <div className="col text-center">
               <h3>
-                <Badge className="text-align-center" variant="secondary">
+                <Badge className="text-align-center mt-4 bg-secondary" variant="secondary">
                   Markdown Input
                 </Badge>
               </h3>
@@ -91,11 +114,11 @@ render(){
               </textarea>
             </div>
           </div>
-          <div className="col-md-6">
+          <div className="col-10 mt-4 shadow-lg">
             {""}
-            <div className="col text-center">
+            <div className="text-center mt-4">
               <h3>
-                <Badge className="text-align-center" variant="secondary">
+                <Badge className="text-align-center bg-secondary" variant="secondary">
                   Preview
                 </Badge>
               </h3>
